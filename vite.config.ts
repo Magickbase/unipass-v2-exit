4b7f6dfa5921d2c37e38e3e1b15c8f0a0e31861a
homura <homura.dev@gmail.com>
2023-10-11 20:13:40 +0800

    fix: fail to build in production

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,31 @@
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import inject from '@rollup/plugin-inject';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    build:
+      mode === 'production'
+        ? {
+            rollupOptions: {
+              plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
+            },
+          }
+        : undefined,
+
+    optimizeDeps: {
+      esbuildOptions: {
+        define: {
+          global: 'globalThis',
+        },
+        plugins:
+          mode === 'development'
+            ? [NodeGlobalsPolyfillPlugin({ buffer: true, process: true })]
+            : undefined,
+      },
     },
-  },
-  define: {
-    global: {},
-  },
+  };
 });
