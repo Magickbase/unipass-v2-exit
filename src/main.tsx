1664aebb1ef7e4b8daf36bec57f3dd0912cb58a5
homura <homura.dev@gmail.com>
2023-10-25 22:44:15 +0800

    refactor: defaults to dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 
+if (
+  typeof localStorage !== 'undefined' &&
+  !localStorage.getItem('chakra-ui-color-mode')
+) {
+  localStorage.setItem('chakra-ui-color-mode', 'dark');
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
